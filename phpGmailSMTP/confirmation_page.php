28626e6e134df7fa0e1cc2ef0113f1889a3f8285
shubh <shubh@gmail.com>
2024-04-28 14:06:14 +0530

    Confirmation Page

---
+++ phpGmailSMTP/confirmation_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Confirmation Page</title>
+</head>
+<body>
+    <h1>Thank you for registering your complaint!</h1>
+    <p>Your complaint has been successfully registered.</p>
+    <p>We will review it and get back to you as soon as possible.</p>
+    
+    <!-- Button to navigate back to index.html# -->
+    <a href="http://localhost/waste-management-system-main/index.html" class="btn btn-primary">Go back to Home</a>
